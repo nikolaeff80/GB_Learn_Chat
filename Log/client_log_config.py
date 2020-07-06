988d397ac9e4386669821dd17aa861588525e18e
nikolaeff80 <nikolaeff2007@mail.ru>
2020-07-06 12:21:43 +0300

    Changing the directory of log files

--- Log/client_log_config.py
+++ Log/client_log_config.py
@@ -1,10 +1,16 @@
 import logging
+import os
 
 client_log = logging.getLogger('client_log')
 
+# create log PATH
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+PATH = os.path.join(PATH, 'client_working.log')
+
 # create the logging file handler
 
-cfh = logging.FileHandler('client_working.log', encoding='utf-8')
+cfh = logging.FileHandler(PATH, encoding='utf-8')
 
 client_log.setLevel(logging.DEBUG)
 myFormatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
--- Log/server_log_config.py
+++ Log/server_log_config.py
@@ -1,23 +1,25 @@
 import logging
+import os
 
 from logging.handlers import TimedRotatingFileHandler
 
 server_log = logging.getLogger('server_log')
 
+# create log PATH
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+PATH = os.path.join(PATH, 'server_working.log')
+
 # create the logging file handler
 
-sfh = logging.FileHandler('server_working.log', encoding='utf-8')
-Rotate_Handler = TimedRotatingFileHandler('server_working.log', when='D', interval=1)
+sfh = logging.FileHandler(PATH, encoding='utf-8')
+Rotate_Handler = TimedRotatingFileHandler(PATH, when='D', interval=1)
 
 server_log.setLevel(logging.DEBUG)
 myFormatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
 sfh.setFormatter(myFormatter)
 
-
 # add handler to logger object
 
 server_log.addHandler(sfh)
 server_log.addHandler(Rotate_Handler)
-
-
-
